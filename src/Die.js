d298e464449a5417442d5858f7ce88e354b65009
Ravi Ramcharan <rsramcharan@gmail.com>
2020-09-17 10:26:35 +0200

    Initial commit

---
+++ src/Die.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Die.css';
+
+class Die extends Component {
+    state = {}
+    render() {
+        return (
+            <i className={`Die fas fa-dice-${this.props.face} ${this.props.rolling ? 'Die-rolling' : ""}`}></i>
+        );
+    }
+}
+
+export default Die;
